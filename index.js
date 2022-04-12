56f2abe2c04401abb40b8b08e4ba60244c56b07b
wale-prog <olapetanwale@outlook.com>
2022-04-12 01:14:43 +0100

    Setup Eslint and check for linter errors

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 const ham = document.querySelector('.ham');
-const navMenu = document.querySelector(".nav-menu");
+const navMenu = document.querySelector('.nav-menu');
 const logo = document.querySelector('.logo');
 
+ham.addEventListener('click', () => {
+  ham.classList.toggle('active');
+  navMenu.classList.toggle('active');
+  logo.classList.toggle('hidden');
+});
 
-ham.addEventListener('click', function () {
-    ham.classList.toggle('active');
-    navMenu.classList.toggle('active');
-    logo.classList.toggle('hidden')
-})
-
-document.querySelectorAll('.nav-menu a').forEach(n => n.addEventListener("click", function () {
-    ham.classList.remove('active');
-    navMenu.classList.remove('active');
-    logo.classList.remove('hidden');
+document.querySelectorAll('.nav-menu a').forEach((n) => n.addEventListener('click', () => {
+  ham.classList.remove('active');
+  navMenu.classList.remove('active');
+  logo.classList.remove('hidden');
 }));
 
-navMenu.addEventListener('click', function () {
-    ham.classList.remove('active');
-    navMenu.classList.remove('active');
-    logo.classList.remove('hidden');
-})
+navMenu.addEventListener('click', () => {
+  ham.classList.remove('active');
+  navMenu.classList.remove('active');
+  logo.classList.remove('hidden');
+});
