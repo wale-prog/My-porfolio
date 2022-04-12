14c1b08d8c80be72d88f400c81eac64c6f0fba4f
wale-prog <olapetanwale@outlook.com>
2022-04-12 10:38:39 +0100

    Adjust elements positioning in the header

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 const ham = document.querySelector('.ham');
 const navMenu = document.querySelector('.nav-menu');
-const logo = document.querySelector('.logo');
 
 ham.addEventListener('click', () => {
   ham.classList.toggle('active');
   navMenu.classList.toggle('active');
-  logo.classList.toggle('hidden');
 });
 
 document.querySelectorAll('.nav-menu a').forEach((n) => n.addEventListener('click', () => {
   ham.classList.remove('active');
   navMenu.classList.remove('active');
-  logo.classList.remove('hidden');
 }));
 
 navMenu.addEventListener('click', () => {
   ham.classList.remove('active');
   navMenu.classList.remove('active');
-  logo.classList.remove('hidden');
 });
