8cc37d6f3612250bffd66940a5d8f7afb547b218
wale-prog <olapetanwale@outlook.com>
2022-04-13 15:32:30 +0100

    Add some .js code

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const ham = document.querySelector('.ham');
 const navMenu = document.querySelector('.nav-menu');
+const blur = document.getElementById('blur');
+const popup1 = document.querySelector('.popup1-btn');
+const close = document.querySelector('.close');
+const popupContent = document.querySelector('#recent-work-modal');
 
 ham.addEventListener('click', () => {
   ham.classList.toggle('active');
@@ -15,3 +19,28 @@ navMenu.addEventListener('click', () => {
   ham.classList.remove('active');
   navMenu.classList.remove('active');
 });
+/* Pop up implementation starts*/
+
+
+
+let closeModal = function () {
+  blur.classList.remove('active');
+  popupContent.classList.remove('active');
+}
+
+let modal = {
+  modalOpen: () => {
+    const modalBody = document.createElement('div');
+    const modalHeader = document.createElement('div');
+    const h1 = document.createElement('h1')
+
+
+    modalBody.appendChild(modalHeader)
+    popupContent.appendChild(modalBody)
+    blur.classList.add('active');
+    popupContent.classList.add('active');
+  }
+}
+
+popup1.addEventListener('click', modal.modalOpen)
+close.addEventListener('click', closeModal);
