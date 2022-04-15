210ef89b291513c1144f270feae4d0567082d964
Masuduzzaman Mohammad <masuduzzaman_811@outlook.com>
2022-04-15 14:37:05 +0600

    add object key-value pairs to modalContent Array

--- index.js
+++ index.js
@@ -124,6 +124,16 @@ projectPortfolio.forEach((cards) => {
 const modalContent = [
   {
     featuredImageDesk: 'images/modal-protfolio.png',
+    featureImageMobile: 'images/modal-desktop.png',
+    featureDiscriptionMob: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    featureDiscriptionDesk: `Lorem Ipsum is simply dummy text of the printing and typesetting
+    industry. Lorem Ipsum
+    has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
+    type
+    and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting
+    industry.
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+    galley of type and scrambled it 1960s with the releax map lapora verita.`,
 
   }
 ]
