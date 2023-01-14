615e0c87a9fcd82cd193dacd7f2faeac619d0609
Olawale <olapetanwale@outlook.com>
2023-01-14 14:21:05 +0100

    Fix project section

---
+++ helper.js
@@ -0,0 +1,30 @@
+
+export const projectData = [
+  {
+    id: 1,
+    title: "FinApp",
+    tech: ["Ruby", "JavaScript", "HTML", "CSS", "PostgreSQL", "RSpec"],
+    image: "images/category_finapp.jpg",
+    imageDesktop: "images/expense_finapp.jpg",
+    description: "FinApp is a web application that allows users to track their expenses and income. Users can create accounts, log in, and create, edit, and delete transactions. FinApp is built with Ruby on Rails and uses PostgreSQL for the database.",
+    modalImage: "images/finapp.jpg",
+    liveImage: "images/modal-live.png",
+    sourceImage: "images/github.png",
+    liveLink: "https://rails-41po.onrender.com",
+    sourceLink: "https://github.com/wale-prog/FinApp",
+  },
+  {
+    id: 2,
+    title: "Deezer Music App",
+    tech: ["React", "Redux", "JavaScript", "HTML", "CSS", "Git", "Jest"],
+    image: "images/deezer-snapshot.jpg",
+    imageDesktop: "images/deezer-snapshot.jpg",
+    description: "Deezer Mobile Music App is an entertainment app that sort through musical chart to bring all time music hits around the world to your doorstep and the fun part is that it comes with preview to the music if for instance the user cannot decide. It is powered by Deezer",
+    modalImage: "images/deezer-snapshot.jpg",
+    liveImage: "images/modal-live.png",
+    sourceImage: "images/github.png",
+    liveLink: "https://cheerful-crostata-ea300b.netlify.app",
+    sourceLink: "https://github.com/wale-prog/deezer-music-app",
+  },
+
+];
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { projectData } from './helper.js';
+
 const ham = document.querySelector('.ham');
 const navMenu = document.querySelector('.nav-menu');
 const portfolioSection = document.getElementById('portfolio');
@@ -23,23 +25,15 @@ const projectRecent = [
     projectNameClass: 'portfolio-heading',
     projectSecClass: 'portfolio-header',
     featuredImageMob: 'images/To-do_list_mobile.png',
-    MobImageClass: 'nodisplay-desktop',
     featuredImageDesk: 'images/To-do_list_desktop.png',
+    MobImageClass: 'nodisplay-desktop',
     projectName: 'To-do List',
     description: 'To-Do list a web app for tracking personal progress through the day. Users can input a list of tasks and mark them as completed once they are done. Built with JavaScript and Webpack',
     technologies: ['HTML', 'CSS', 'JavaScript', 'Webpack'],
   },
 ];
 
-const projectPortfolio = [
-  {
-    mobileHeading: 'Profesional Art Printing Data',
-    desktopHeadingHealth: 'Data Dashboard Healthcare',
-    desktopHeadingWebsite: 'Website Portfolio',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard.',
-    technologies: ['html', 'bootstrap', 'Ruby'],
-  },
-];
+
 
 const portfolio = document.createElement('section');
 portfolio.id = 'portfolio';
@@ -57,10 +51,7 @@ projectRecent.forEach((recent) => {
     <h2>${recent.projectName}</h2>
     <p class="portfolio-text">${recent.description}</p>
     <ul class="skill-list">
-      <li>${recent.technologies[0]}</li>
-      <li>${recent.technologies[1]}</li>
-      <li>${recent.technologies[2]}</li>
-      <li>${recent.technologies[3]}</li>
+      ${recent.technologies.map((tech) => `<li>${tech}</li>`).join('')}
     </ul>
     <a href="#portfolio" class="btn skill-list-btn popup1-btn">See Project</a>
   </div>
@@ -68,46 +59,25 @@ projectRecent.forEach((recent) => {
   portfolioSection.appendChild(portfolio);
 });
 
-projectPortfolio.forEach((cards) => {
-  function cardInstancefunc(visibility, btnClass, heading, heading1Class) {
-    return `
-      <div class="${visibility}">
-        <h2 class="${heading1Class}">${cards.mobileHeading}</h2>
-        ${heading}
-        <p>${cards.description}</p>
-        <ul class="skill-list printing-list">
-          <li>${cards.technologies[0]}</li>
-          <li>${cards.technologies[1]}</li>
-          <li>${cards.technologies[2]}</li>
-        </ul>
-      <a href="#" class="btn_2 ${btnClass} popup1-btn">See Project</a>
-      </div>
-      `;
-  }
-  for (let i = 1; i <= 6; i += 1) {
-    if (i === 1) {
-      const layout = document.createElement('div');
-      const firstCard = document.querySelector(`.card${i}`);
-      layout.innerHTML = cardInstancefunc('nonvisible', 'deezer', '', '');
-      firstCard.appendChild(layout);
-    } else if (i === 2 || i === 5) {
-      const layout = document.createElement('div');
-      const secondCard = document.querySelector(`.card${i}`);
-      layout.innerHTML = cardInstancefunc('', 'nodisplay-desktop', '<h2 class="hidden-mobile">Data Dashboard Healthcare</h2>', 'nodisplay-desktop');
-      secondCard.appendChild(layout);
-    } else if (i === 3 || i === 6) {
-      const layout = document.createElement('div');
-      const thirdCard = document.querySelector(`.card${i}`);
-      layout.innerHTML = cardInstancefunc('', 'nodisplay-desktop', '<h2 class="hidden-mobile">Website Portfolio</h2>', 'nodisplay-desktop');
-      thirdCard.appendChild(layout);
-    } else {
-      const layout = document.createElement('div');
-      const fourthCard = document.querySelector(`.card${i}`);
-      layout.innerHTML = cardInstancefunc('', 'nodisplay-desktop', '', '');
-      fourthCard.appendChild(layout);
-    }
-  }
+projectData.forEach((project, index) => {
+  const projects = document.createElement('div');
+  projects.classList.add('project');
+  const projectsContainer = document.querySelector('.grid-container')
+  projects.innerHTML = `
+    <h3 class="project-title">${project.title}</h3>
+    <ul class="project-tech">
+      <em>built with => </em>
+      ${project.tech.map((tech) => `<li>${tech}</li>`).join('')}
+    </ul>
+    <img class="project-image nodisplay-desktop" src=${project.image} alt=${project.title}/>
+    <img class="project-image hidden-mobile" src=${project.imageDesktop} alt=${project.title}/>
+    <p>${project.description}</p>
+    <button id=${project.id} class="btn skill-list-btn project-popup">See Project</button>
+  `;
+  projects.classList.add(`card${index + 1}`)
+  projectsContainer.appendChild(projects)
 });
+
 
 const modalContent = [
   {
@@ -121,20 +91,20 @@ const modalContent = [
     liveLink: 'https://wale-prog.github.io/To-do-List-Project/',
     sourceLink: 'https://github.com/wale-prog/My-porfolio',
   },
-
 ];
-const deezerModal = [
-    {
-    projectName: 'Deezer Music App',
-    featuredImageDesk: '<img class="hidden-mobile modal-desktop" src="images/DeezerApp-detail.jpeg" alt="">',
-    closeBtn: 'images/modal-close.png',
-    featureDescriptionMob: 'Deezer Mobile Music App" is an entertainment app that sort through musical chart to bring all time music hits around the world to your doorstep and the fun part is that it comes with preview to the music if for instance the user cannot decide. It is powered by Deezer',
-    featureDescriptionDesk: 'Deezer Mobile Music App" is an entertainment app that sort through musical chart to bring all time music hits around the world to your doorstep and the fun part is that it comes with preview to the music if for instance the user cannot decide. It is powered by Deezer',
-    technologies: ['html', 'css', 'React', 'Redux', 'Git & Github'],
-    liveLink: 'https://cheerful-crostata-ea300b.netlify.app/',
-    sourceLink: 'https://github.com/wale-prog/deezer-music-app',
-  },
-]
+
+// const deezerModal = [
+//     {
+//     projectName: 'Deezer Music App',
+//     featuredImageDesk: '<img class="hidden-mobile modal-desktop" src="images/DeezerApp-detail.jpeg" alt="">',
+//     closeBtn: 'images/modal-close.png',
+//     featureDescriptionMob: 'Deezer Mobile Music App" is an entertainment app that sort through musical chart to bring all time music hits around the world to your doorstep and the fun part is that it comes with preview to the music if for instance the user cannot decide. It is powered by Deezer',
+//     featureDescriptionDesk: 'Deezer Mobile Music App" is an entertainment app that sort through musical chart to bring all time music hits around the world to your doorstep and the fun part is that it comes with preview to the music if for instance the user cannot decide. It is powered by Deezer',
+//     technologies: ['html', 'css', 'React', 'Redux', 'Git & Github'],
+//     liveLink: 'https://cheerful-crostata-ea300b.netlify.app/',
+//     sourceLink: 'https://github.com/wale-prog/deezer-music-app',
+//   },
+// ]
 
 modalContent.forEach((modal) => {
   const modalGen = document.getElementById('recent-work-modal');
@@ -149,7 +119,7 @@ modalContent.forEach((modal) => {
   <ul class="skill-list">
   ${modal.technologies.map((tech) => (
     `<li>${tech}</li>`
-  ))}
+  )).join('')}
   </ul>
   <div class="modal-flex">
       ${modal.featureImageMobile}
@@ -170,38 +140,38 @@ modalContent.forEach((modal) => {
   modalGen.appendChild(modalBody);
 });
 
-deezerModal.forEach((modal) => {
-  const modalGen = document.getElementById('deezer');
-  const modalBody = document.createElement('div');
-  modalBody.classList.add('modal-body');
-  modalBody.innerHTML = `
-  <div class="modal-header">
-    <h1 class="nodisplay-desktop">${modal.projectName}</h1>
-    <h1 class="hidden-mobile">Track your daily milestones and achievments</h1>
-    <img class="close1" src="${modal.closeBtn}" alt="close modal button">
-  </div>
-  <ul class="skill-list">
-  ${modal.technologies.map((tech) => 
-  `<li>${tech}</li>`
-  )}
-  </ul>
-  <div class="modal-flex">
-      ${modal.featuredImageDesk}
-    <div>
-        <p class="nodisplay-desktop modal-para-text">${modal.featureDescriptionMob}</P>
-        <p class="hidden-mobile modal-para-text">${modal.featureDescriptionDesk}</p>
+// deezerModal.forEach((modal) => {
+//   const modalGen = document.getElementById('deezer');
+//   const modalBody = document.createElement('div');
+//   modalBody.classList.add('modal-body');
+//   modalBody.innerHTML = `
+//   <div class="modal-header">
+//     <h1 class="nodisplay-desktop">${modal.projectName}</h1>
+//     <h1 class="hidden-mobile">Track your daily milestones and achievments</h1>
+//     <img class="close1" src="${modal.closeBtn}" alt="close modal button">
+//   </div>
+//   <ul class="skill-list">
+//   ${modal.technologies.map((tech) => 
+//   `<li>${tech}</li>`
+//   )}
+//   </ul>
+//   <div class="modal-flex">
+//       ${modal.featuredImageDesk}
+//     <div>
+//         <p class="nodisplay-desktop modal-para-text">${modal.featureDescriptionMob}</P>
+//         <p class="hidden-mobile modal-para-text">${modal.featureDescriptionDesk}</p>
     
-        <div class="modal-btn-container">
-          <ul class="modal-buttons">
-            <li class="first-btn"><a class="live-btn" href="${modal.liveLink}" target="_blank">See Live <img src="images/modal-live.png" alt="Live project page"></a></li>
-            <li class="second-btn"><a class="source-btn" href="${modal.sourceLink}" target="_blank">See Source <img src="images/modal-git.png" alt="Link to source code"></a></li>
-          </ul>
-        </div>
-    </div>
-  </div>
-  `;
-  modalGen.appendChild(modalBody);
-});
+//         <div class="modal-btn-container">
+//           <ul class="modal-buttons">
+//             <li class="first-btn"><a class="live-btn" href="${modal.liveLink}" target="_blank">See Live <img src="images/modal-live.png" alt="Live project page"></a></li>
+//             <li class="second-btn"><a class="source-btn" href="${modal.sourceLink}" target="_blank">See Source <img src="images/modal-git.png" alt="Link to source code"></a></li>
+//           </ul>
+//         </div>
+//     </div>
+//   </div>
+//   `;
+//   modalGen.appendChild(modalBody);
+// });
 
 /* Pop up implementation starts */
 const blur = document.getElementById('blur');
@@ -209,9 +179,10 @@ const popupContent = document.getElementById('recent-work-modal');
 const close = document.querySelector('.close');
 const popup1 = document.querySelectorAll('.popup1-btn');
 
+
 const modalOpen = () => {
   blur.classList.add('active');
-  popupContent.classList.add('active');
+  popupContent.classList.add('active');  
 };
 
 const closeModal = () => {
@@ -224,18 +195,51 @@ for (let i = 0; i < popup1.length; i += 1) {
 }
 close.addEventListener('click', closeModal);
 
-const deezerProject = document.querySelector('.card1')
-const deezerPopup = document.getElementById('deezer')
-const close1 = document.querySelector('.close1');
+const btnClicked = document.querySelectorAll('.btn');
+const modalDiv = document.querySelector('.project-modal')
+for (let i = 0; i < btnClicked.length; i += 1) {
+  btnClicked[i].addEventListener('click', (event) => {
+   const projectId = +event.target.id;
+   if (projectId) {
+    const project = projectData.find((project) => project.id === projectId)
+    const projects = document.createElement('div');
+    projects.classList.add('modal-project-body');
+    blur.classList.add('active');
+    projects.innerHTML = `
+    <div class="modal-header">
+      <h3 class="project-title">${project.title}</h3>
+      <img class="close-modal" src="images/modal-close.png" alt="close modal button">
+    </div>
+    <ul class="project-tech">
+      <em>built with &#8658;</em>
+      ${project.tech.map((tech) => `<li>${tech}</li>`).join('')}
+    </ul>
+    <div class="modal-img">
+      <div>
+        <img class="project-image-modal nodisplay-desktop" src=${project.image} alt=${project.title}/>
+        <img class="project-image-modal hidden-mobile" src=${project.imageDesktop} alt=${project.title}/>
+      </div>
+      <p>${project.description}</p>
+    </div>
+    <div class="icon-img">
+      <a href=${project.liveLink} target=__blank class="btn skill-list-btn">See Live<img src=${project.liveImage} alt="Live Icon" /> </a>
+      <a href=${project.sourceLink} class="btn skill-list-btn">See Source <img src=${project.sourceImage} alt="Live Icon" /> </a>
+    </div>
+    `;
+    modalDiv.appendChild(projects)
+    document.querySelectorAll('.project-popup').forEach((project) => {
+      project.addEventListener('click', () => {
+        document.querySelector('.modal-project-body').classList.add('active');
+      })
+    })
+    document.querySelector('.close-modal').addEventListener('click', () => {
+      blur.classList.remove('active');
+      const projectModal = document.querySelector('.modal-project-body');
+      const projectModalParent = projectModal.parentNode;
+      projectModalParent.removeChild(projectModal);
+    })
+   }
+  })
+};
 
-const closeDeezerModal = () => {
-  blur.classList.remove('active');
-  deezerPopup.classList.remove('active');
-}
 
-const openDeezerModal = () => {
-  blur.classList.add('active');
-  deezerPopup.classList.add('active');
-}
-deezerProject.addEventListener('click', openDeezerModal);
-close1.addEventListener('click', closeDeezerModal);
